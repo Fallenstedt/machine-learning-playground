ac34ed52f6a28281efb3f1f9235938088906ecaf
Alex Fallenstedt <a.fallenstedt@gmail.com>
2023-07-17 21:41:08 -0700

    obtain ind and dep variables

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,18 @@
-import numpy as np
+import numpy as np # works well with arrays
+import matplotlib.pyplot as plt # plots charts
+import pandas as pd # pandas will import data
 
+# import the dataset
+dataset = pd.read_csv('./data.csv')
 
-a = np.arange(6)
+# create matrix of features (indepdendent) and dependent variable vector
+# features are columns which we will use to predict the dependent variable
 
-print(a)
+# x is matrix of features
+# get all the rows, and all the columns except the last one
+x = dataset.iloc[:, :-1].values
+
+# y is the vector of depdendent variables
+# get all the rows for the last column
+y = dataset.iloc[:, -1].values
+print(y)
